dc5a82f5c2093a0e94df7b021894e0610d150b53
mandest <mandest@gmail.com>
2014-10-04 23:31:51 -0500

    Added gitignore updating to the install script.

--- install.ps1
+++ install.ps1
@@ -36,23 +36,36 @@ function Extract-File {
   }
 }
 
+function Get-Gitignore{
+  param(
+    [System.IO.DirectoryInfo]
+    $CurrentDirectory
+  )
+  if($CurrentDirectory -ne $null) {
+    if(Test-Path ("$CurrentDirectory\.gitignore"))
+    {
+      return "$CurrentDirectory\.gitignore"
+    }
+
+    Get-Gitignore $CurrentDirectory.Parent
+  }
+}
+
 #######################
 ###### EXECUTION ######
 #######################
 
-Write-Progress -Activity "Installing Vagrunt" -Status "Testing for 'build.sh'" -PercentComplete 0
-if(Test-Path "vagrunt.ps1")
-{
+Write-Progress -Activity "Installing Vagrunt" -Status "Testing for existing vagrunt" -PercentComplete 0
+if(Test-Path "vagrunt.ps1") {
   Write-Host "It appears that vagrunt is already installed."
   Write-Host -NoNewLine "Forcefully continue anyway? [y/N] "
   $answer = Read-Host
-  if($answer -notmatch "y")
-  {
+  if($answer -notmatch "y") {
     return
   }
 }
-if(-not (Test-Path "./build.sh"))
-{
+Write-Progress -Activity "Installing Vagrunt" -Status "Testing for 'build.sh'" -PercentComplete 5
+if(-not (Test-Path "./build.sh")) {
   Write-Host "It appears there is no build.sh in this directory."
   Write-Host -NoNewLine "Are you sure you want to install Vagrunt? [y/N] "
   $answer = Read-Host
@@ -62,20 +75,21 @@ if(-not (Test-Path "./build.sh"))
   }
 }
 
-# download the package
+# Download
 Write-Progress -Activity "Installing Vagrunt" -Status "Downloading Files" -PercentComplete 20
 $zipFile = "$($env:TEMP)\vagrunt.zip"
 Download-File -Url "https://github.com/mandest/vagrunt/archive/master.zip" -File $zipFile
 
+# Unzip
 Write-Progress -Activity "Installing Vagrunt" -Status "Unpacking Files" -PercentComplete 50
 $tempPath = "$($env:TEMP)\vagrunt"
-if(Test-Path $tempPath)
-{
+if(Test-Path $tempPath) {
   $null = Remove-Item -Recurse -Force $tempPath
 }
 $null = New-Item -Type Directory $tempPath
 Extract-File -File $zipFile -OutputDirectory $tempPath
 
+# Copy
 Write-Progress -Activity "Installing Vagrunt" -Status "Copying Files" -PercentComplete 70
 Get-ChildItem "$tempPath\vagrunt-master" | ? { 
   $ignoredFiles -notcontains $_.Name
@@ -83,10 +97,24 @@ Get-ChildItem "$tempPath\vagrunt-master" | ? {
   Copy-Item $_.FullName "./$($_.Name)"
 }
 
+# Update .gitignore
+Write-Progress -Activity "Installing Vagrunt" -Status "Updating .gitignore if present" -PercentComplete 80
+$gitignore = Get-Gitignore -CurrentDirectory "./"
+if($gitignore -ne $null) {
+  if((Get-Content $gitignore | ? {$_ -match "/puppet/modules/"}) -eq 0) {
+    "`n**/puppet/modules/" | Out-File -Append $gitignore
+  }
+  if((Get-Content $gitignore | ? {$_ -match ".vagrant"}) -eq 0) {
+    ".vagrant" | Out-File -Append $gitignore
+  }
+}
+
+# Cleanup
 Write-Progress -Activity "Installing Vagrunt" -Status "Cleaning Up" -PercentComplete 90
 $null = Remove-Item -Recurse -Force $zipFile
 $null = Remove-Item -Recurse -Force $tempPath
 
+# Notify
 Write-Host ""
 Write-Host -ForegroundColor Cyan "Finished!"
 Write-Host "Use ./vagrunt.ps1 to run vagrunt.  Happy grunting!"
