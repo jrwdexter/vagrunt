e0fd5c8d2d45254e53de98800411225090dbd37a
mandest <mandest@gmail.com>
2014-10-04 23:09:11 -0500

    Updated the install script.

--- install.ps1
+++ install.ps1
@@ -4,10 +4,10 @@
 $rootUrl = "https://github.com/mandest/vagrunt/master.zip"
 $downloader = New-Object System.Net.WebClient
 $ignoredFiles = @(
-  "./LICENSE",
-  "./README.md",
-  "./install.ps1",
-  "./.gitignore"
+  "LICENSE",
+  "README.md",
+  "install.ps1",
+  ".gitignore"
 )
 
 #######################
@@ -29,11 +29,10 @@ function Extract-File {
     [string]$OutputDirectory
   )
   $shell = new-object -com shell.application
-  $zip = $shell.NameSpace($File)
+  $zip = $shell.Namespace($File)
   foreach($item in $zip.items())
   {
-    Write-Host $item
-    $shell.Namespace($OutputDirectory).CopyHere($item)
+    $null = $shell.Namespace($OutputDirectory).CopyHere($item)
   }
 }
 
@@ -42,10 +41,20 @@ function Extract-File {
 #######################
 
 Write-Progress -Activity "Installing Vagrunt" -Status "Testing for 'build.sh'" -PercentComplete 0
+if(Test-Path "vagrunt.ps1")
+{
+  Write-Host "It appears that vagrunt is already installed."
+  Write-Host -NoNewLine "Forcefully continue anyway? [y/N] "
+  $answer = Read-Host
+  if($answer -notmatch "y")
+  {
+    return
+  }
+}
 if(-not (Test-Path "./build.sh"))
 {
   Write-Host "It appears there is no build.sh in this directory."
-  Write-Host "Are you sure you want to install Vagrunt? [y/N]"
+  Write-Host -NoNewLine "Are you sure you want to install Vagrunt? [y/N] "
   $answer = Read-Host
   if($answer -notmatch "y")
   {
@@ -55,12 +64,30 @@ if(-not (Test-Path "./build.sh"))
 
 # download the package
 Write-Progress -Activity "Installing Vagrunt" -Status "Downloading Files" -PercentComplete 20
-$file = "$($env:TEMP)/$File"
-Download-File -Url "https://github.com/mandest/vagrunt/master.zip" -File $file
+$zipFile = "$($env:TEMP)\vagrunt.zip"
+Download-File -Url "https://github.com/mandest/vagrunt/archive/master.zip" -File $zipFile
 
-Write-Progress -Activity "Installing Vagrunt" -Status "Unpacking Files" -PercentComplete 70
-Extract-File -File $file -OutputDirectory "./"
+Write-Progress -Activity "Installing Vagrunt" -Status "Unpacking Files" -PercentComplete 50
+$tempPath = "$($env:TEMP)\vagrunt"
+if(Test-Path $tempPath)
+{
+  $null = Remove-Item -Recurse -Force $tempPath
+}
+$null = New-Item -Type Directory $tempPath
+Extract-File -File $zipFile -OutputDirectory $tempPath
 
-Write-Host -ForegroundColor "Finished!"
+Write-Progress -Activity "Installing Vagrunt" -Status "Copying Files" -PercentComplete 70
+Get-ChildItem "$tempPath\vagrunt-master" | ? { 
+  $ignoredFiles -notcontains $_.Name
+} | % {
+  Copy-Item $_.FullName "./$($_.Name)"
+}
+
+Write-Progress -Activity "Installing Vagrunt" -Status "Cleaning Up" -PercentComplete 90
+$null = Remove-Item -Recurse -Force $zipFile
+$null = Remove-Item -Recurse -Force $tempPath
+
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Finished!"
 Write-Host "Use ./vagrunt.ps1 to run vagrunt.  Happy grunting!"
 Write-Host ""
